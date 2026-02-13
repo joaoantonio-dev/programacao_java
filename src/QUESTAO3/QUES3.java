0bbbd74017bc464a64bf92e5bae037ade7ccd9fe
João Antônio <tontjoao@gmail.com>
2026-02-13 12:04:32 -0300

    Update QUES3.java

--- src/QUESTAO3/QUES3.java
+++ src/QUESTAO3/QUES3.java
@@ -1,11 +1,9 @@
-package QUESTAO3;
-
 public class QUES3 {
     public static void main(String[] args) {
-        int num1 = 10;
-        int num2 = 90;
-        int num3 = 90;
-        int media;
+        double num1 = 10.00;
+        double num2 = 90.00;
+        double num3 = 90.00;
+        double media;
         media = (num1 + num2 + num3) / 3;
         if (media>=70){
             System.out.println("Aprovado");
